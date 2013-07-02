c43d2df9949b11874688456c47a4c48bd611ccc9
Pramod G. Menon <pramodgmenon@gmail.com>
2013-07-02 14:40:38 +0530

    Initial Commit

---
+++ admin/config_sample.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\webserver\www\opencart/admin/');
+define('DIR_SYSTEM', 'C:\webserver\www\opencart/system/');
+define('DIR_DATABASE', 'C:\webserver\www\opencart/system/database/');
+define('DIR_LANGUAGE', 'C:\webserver\www\opencart/admin/language/');
+define('DIR_TEMPLATE', 'C:\webserver\www\opencart/admin/view/template/');
+define('DIR_CONFIG', 'C:\webserver\www\opencart/system/config/');
+define('DIR_IMAGE', 'C:\webserver\www\opencart/image/');
+define('DIR_CACHE', 'C:\webserver\www\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\webserver\www\opencart/download/');
+define('DIR_LOGS', 'C:\webserver\www\opencart/system/logs/');
+define('DIR_CATALOG', 'C:\webserver\www\opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config_sample.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\webserver\www\opencart/catalog/');
+define('DIR_SYSTEM', 'C:\webserver\www\opencart/system/');
+define('DIR_DATABASE', 'C:\webserver\www\opencart/system/database/');
+define('DIR_LANGUAGE', 'C:\webserver\www\opencart/catalog/language/');
+define('DIR_TEMPLATE', 'C:\webserver\www\opencart/catalog/view/theme/');//select theme
+define('DIR_CONFIG', 'C:\webserver\www\opencart/system/config/');
+define('DIR_IMAGE', 'C:\webserver\www\opencart/image/');
+define('DIR_CACHE', 'C:\webserver\www\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\webserver\www\opencart/download/');
+define('DIR_LOGS', 'C:\webserver\www\opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
